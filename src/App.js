72c884a364f4793e73505daffeaada360be7d610
Pedro Barão <phbarao@gmail.com>
2021-10-13 18:11:15 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Modal from "./components/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  function handleShowMessageClick() {
+    setShowModal(true);
+  }
+
+  function handleCloseModal() {
+    setShowModal(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div
+      style={{
+        height: "100%",
+        display: "grid",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: 400,
+          position: "relative",
+        }}
+      >
+        <h1>Modal using Portals</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nihil
+          commodi neque reprehenderit, vel possimus omnis libero! Maxime
+          laudantium blanditiis cumque libero facere consequuntur eligendi
+          deserunt. Laboriosam numquam cumque id!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <button onClick={handleShowMessageClick}>Show Modal</button>
+        {showModal ? (
+          <Modal onClose={handleCloseModal}>This is the modal message!</Modal>
+        ) : null}
+      </div>
     </div>
   );
 }
